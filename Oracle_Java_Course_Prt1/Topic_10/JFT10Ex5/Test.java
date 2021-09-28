1cc53e19b69729d6baaeda0ba9e80b7b9724b478
Al-Murphy101 <82583003+Al-Murphy101@users.noreply.github.com>
2021-09-28 10:01:02 +0100

    Delete Oracle_Java_Course_Prt1 directory

--- Oracle_Java_Course_Prt1/Topic_10/JFT10Ex5/Test.java
+++
@@ -1,8 +0,0 @@
-public class Test
-{
-	public static void main (String[] args)
-	{
-		NumberRange number = new NumberRange();
-		System.out.println(number.checkInRange(5,500,501));
-	}
-}
--- Oracle_Java_Course_Prt1/Topic_10/JFT10Ex6/Test.java
+++
@@ -1,8 +0,0 @@
-public class Test
-{
-	public static void main (String[] args)
-	{
-		VowelCount vc = new VowelCount();
-		System.out.println(vc.vowelCount("Walking on the moon"));
-	}
-}
